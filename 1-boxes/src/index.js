0bb5995ac643361a06691b98becd2ee18dc1450b
ahmedfahadpk <57640040+ahmedfahadpk@users.noreply.github.com>
2020-03-23 22:00:41 +0200

    tested a component with html render on local node server

--- 1-boxes/src/index.js
+++ 1-boxes/src/index.js
@@ -2,3 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Person(props) {
+  return (
+    <div className="box">
+      <h1>{props.name}</h1>
+      <p>Title: {props.title}</p>
+      <p>Age: {props.age}</p>
+    </div>
+    
+  );
+};
+
+const app = (
+  <div>
+    <Person name="Maria" title="CEO" age="39" />
+    <Person name="Kati" title="developer" age="25" />
+    <Person name="Karin" title="designer" age="45" />
+  </div>
+)
+
+ReactDOM.render(app, document.querySelector("#root"));
