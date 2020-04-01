537c71ca757f63a4b1c31ecc3d705325610a2d65
ahmedfahadpk <57640040+ahmedfahadpk@users.noreply.github.com>
2020-04-01 11:42:22 +0300

    init folder structure

--- 4-forms/src/App.js
+++ 4-forms/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
     state = {
         value: "default load"
     };
 
-    eventHandler () => {
+    eventHandler = () => {
 
 
     };
