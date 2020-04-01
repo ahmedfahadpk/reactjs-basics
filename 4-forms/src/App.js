0fb9355779d5ecce5409911497f37736b81853d6
ahmedfahadpk <57640040+ahmedfahadpk@users.noreply.github.com>
2020-04-01 11:03:39 +0300

    init

---
+++ 4-forms/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        value: "default load"
+    };
+
+    eventHandler () => {
+
+
+    };
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default App;
